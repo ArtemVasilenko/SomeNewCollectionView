1e99de8fd2e8c8461d26243f3077dda11ef31cc7
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-19 17:06:18 +0300

    +

--- SomeNewCollectionView/MyCollectionViewCell.swift
+++ SomeNewCollectionView/MyCollectionViewCell.swift
@@ -1,13 +1,8 @@
-//
-//  MyCollectionViewCell.swift
-//  SomeNewCollectionView
-//
-//  Created by Артем on 6/19/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
 class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var newLabel: UILabel!
     
 }
