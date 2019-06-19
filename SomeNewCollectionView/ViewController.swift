f4d4015a4ad5e7f7428f11393edfb99a5aca64cf
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-06-19 16:47:07 +0300

    start

--- SomeNewCollectionView/ViewController.swift
+++ SomeNewCollectionView/ViewController.swift
@@ -1,18 +1,11 @@
-//
-//  ViewController.swift
-//  SomeNewCollectionView
-//
-//  Created by Артем on 6/19/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
